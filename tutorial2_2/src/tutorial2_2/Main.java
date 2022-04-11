c1f83478bf842df8ff9e9c321fc774fc0ebf972f
mororong9 <kangmoonju0603@gmail.com>
2022-04-12 05:40:53 +0900

    format change

---
+++ tutorial2_2/src/tutorial2_2/Main.java
@@ -0,0 +1,11 @@
+package tutorial2_2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a=200;
+		System.out.format("8진수:%o\n", a);
+		System.out.format("16진수:%x",a);
+	}
+
+}
