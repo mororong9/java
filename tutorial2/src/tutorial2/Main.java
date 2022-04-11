29ca7d0802809e8b08c6cb00ce42cb685603cae3
mororong9 <kangmoonju0603@gmail.com>
2022-04-12 01:42:17 +0900

    first java commit

---
+++ tutorial2/src/tutorial2/Main.java
@@ -0,0 +1,16 @@
+package tutorial2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intType =100;
+		double doubleType=150.5;
+		String stringType="¸ð·Î·ÕÁö";
+		
+		System.out.println(intType);
+		System.out.println(doubleType);
+		System.out.println(stringType);
+	}
+
+}
